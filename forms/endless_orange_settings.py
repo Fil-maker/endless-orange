a9d0d8aa1896ff125e82285644a4fb21aabe50c8
sssemion <ssemi.on@yandex.ru>
2020-09-21 20:47:16 +0500

    Добавлена возможность выбора РТВ+ТРИЗ

--- forms/endless_orange_settings.py
+++ forms/endless_orange_settings.py
@@ -8,5 +8,5 @@ class EndlessOrangeSettingsForm(FlaskForm):
     mode = HiddenField(validators=[DataRequired(), AnyOf(["endless-orange"])])
     time = IntegerRangeField(validators=[DataRequired(), NumberRange(min=1, max=5)])
     rounds = IntegerRangeField(validators=[DataRequired(), NumberRange(min=1, max=20)])
-    question_type = RadioField(choices=["dci", "tips"], validators=[DataRequired()])
-    communication_type = RadioField(choices=["host", "both", "team"], validators=[DataRequired()])
+    question_type = RadioField(choices=["dci", "both-dci-tips", "tips"], validators=[DataRequired()])
+    communication_type = RadioField(choices=["host", "both-host-team", "team"], validators=[DataRequired()])
